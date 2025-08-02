577b75cf849486ee2bbe9d63b49d61f710bf333e
Kelll31 <kuprikov.kelll31@gmail.com>
2025-08-02 16:30:28 +0300

    Only light version

--- src/components/layout/Header/index.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/layout/Header/index.tsx
-export { Header } from './Header';
-export { Navigation } from './Navigation';
-export { ThemeToggle } from './ThemeToggle';
-export { StatusTooltip } from './StatusTooltip';
-
-export type { NavigationItem } from './Navigation';
-export type { StatusTooltipProps } from './StatusTooltip';
